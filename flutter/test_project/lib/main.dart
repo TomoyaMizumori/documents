5ee067e5d1e7ea159f655c2812fb06f84675c142
TomoyaMizumori <tomoya_mizumori@dema.co.jp>
2025-02-18 07:29:12 +0900

    ウィジェットの切り出し

--- flutter/test_project/lib/main.dart
+++ flutter/test_project/lib/main.dart
@@ -85,62 +85,7 @@ class MyHomePage extends StatelessWidget {
               'Calendar',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
             ),
-            Expanded(
-                child: ListView.builder(
-              itemCount: dateList.length,
-              itemBuilder: (context, index) {
-                final element = dateList[index];
-                final weekday = weekdayFunction(index);
-                var textColor = Colors.black;
-                if (weekday == 'Sat') {
-                  textColor = Colors.blue;
-                }
-                if (weekday == 'Sun') {
-                  textColor = Colors.red;
-                }
-                if (index == 0) {
-                  return ListTile(
-                    leading: Container(
-                      width: 60,
-                      height: 60,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle, color: Colors.green),
-                      child: const Center(
-                        child: Text(
-                          'Today',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 16,
-                              color: Colors.white),
-                        ),
-                      ),
-                    ),
-                    onTap: () =>
-                        debugPrint('selected ${element.month}/${element.day}'),
-                    title: Text(
-                      '${weekday} ${element.month}/${element.day}',
-                      style: TextStyle(color: textColor),
-                    ),
-                    subtitle: const Text('schedule:'),
-                  );
-                } else {
-                  return ListTile(
-                    leading: const Icon(
-                      Icons.arrow_forward,
-                      size: 60,
-                      color: Colors.blue,
-                    ),
-                    title: Text(
-                      '${weekday} ${element.month}/${element.day}',
-                      style: TextStyle(color: textColor),
-                    ),
-                    subtitle: const Text('schedule:'),
-                    onTap: () =>
-                        debugPrint('selected ${element.month}/${element.day}'),
-                  );
-                }
-              },
-            ))
+            Calendar(dateList: dateList)
           ],
         ),
       ),
@@ -150,6 +95,73 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Colors.blue,
       ),
     );
+  }
+}
+
+class Calendar extends StatelessWidget {
+  const Calendar({
+    super.key,
+    required this.dateList,
+  });
+
+  final List dateList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: ListView.builder(
+      itemCount: dateList.length,
+      itemBuilder: (context, index) {
+        final element = dateList[index];
+        final weekday = weekdayFunction(index);
+        var textColor = Colors.black;
+        if (weekday == 'Sat') {
+          textColor = Colors.blue;
+        }
+        if (weekday == 'Sun') {
+          textColor = Colors.red;
+        }
+        if (index == 0) {
+          return ListTile(
+            leading: Container(
+              width: 60,
+              height: 60,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.green),
+              child: const Center(
+                child: Text(
+                  'Today',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+            onTap: () => debugPrint('selected ${element.month}/${element.day}'),
+            title: Text(
+              '${weekday} ${element.month}/${element.day}',
+              style: TextStyle(color: textColor),
+            ),
+            subtitle: const Text('schedule:'),
+          );
+        } else {
+          return ListTile(
+            leading: const Icon(
+              Icons.arrow_forward,
+              size: 60,
+              color: Colors.blue,
+            ),
+            title: Text(
+              '${weekday} ${element.month}/${element.day}',
+              style: TextStyle(color: textColor),
+            ),
+            subtitle: const Text('schedule:'),
+            onTap: () => debugPrint('selected ${element.month}/${element.day}'),
+          );
+        }
+      },
+    ));
   }
 }
 
