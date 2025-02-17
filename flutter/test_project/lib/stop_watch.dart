56c2aeb6c5a3dd5ee646fd57c89364bdc0f0d7ba
TomoyaMizumori <tomoya_mizumori@dema.co.jp>
2025-02-18 07:46:33 +0900

    各ウィジェットの切り出し

--- flutter/test_project/lib/stop_watch.dart
+++ flutter/test_project/lib/stop_watch.dart
@@ -21,3 +21,63 @@ Future<Duration> stopWatch(int maxMin) async {
   }
   return time;
 }
+
+class StopWatch extends StatelessWidget {
+  const StopWatch({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Container(
+          width: 160,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10))),
+            onPressed: () {
+              if (!start) {
+                setStart = true;
+                stopWatch(10);
+              }
+            },
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.start,
+                  color: Colors.white,
+                ),
+                Text('START', style: TextStyle(color: Colors.white))
+              ],
+            ),
+          ),
+        ),
+        Container(
+          width: 160,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10))),
+            onPressed: () => {setStart = false},
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.timelapse,
+                  color: Colors.white,
+                ),
+                Text('STOP', style: TextStyle(color: Colors.white))
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
