6a2da023e5a6bd03784b3d28319d77b2a24af218
TomoyaMizumori <tomoya_mizumori@dema.co.jp>
2025-02-18 15:51:48 +0900

    TODO appbar, bottomBar

---
+++ flutter/todo_practice/lib/todo_list.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatefulWidget {
+  const TodoList({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _TodoList createState() => _TodoList();
+}
+
+class _TodoList extends State<TodoList> {
+  int bottomIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('TODO LIST')),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.unpublished), label: '未完了'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.check_circle_outlined), label: '完了'),
+        ],
+        onTap: (final value) => {
+          setState(() {
+            bottomIndex = value;
+          })
+        },
+        currentIndex: bottomIndex,
+      ),
+    );
+  }
+}
